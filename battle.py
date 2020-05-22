9045db68c2c8b3b11b7574236fedcf0b7d36b741
EnkanSakura <EnkanSakura@users.noreply.github.com>
2020-05-22 17:42:13 +0800

    Update battle.py

--- battle.py
+++ battle.py
@@ -12,7 +12,7 @@ def change():   #换人
     touch([1931, 101])
     sleep(2.5)
     
-def spellcard(no):  #符卡，no为释放卡的次序
+def spellcard(no):  #符卡，no为卡的序号
     touch([360, 706])
     sleep(0.8)
     if no == 1:
@@ -64,7 +64,7 @@ def shoot(s):   #弹幕，s为扩散1或集中2
         touch([1381, 877])
     sleep(0.5)
 
-def run_away():
+def run_away(): #离开战斗（逃げるだよ）
     touch([1193, 38])
     sleep(0.3)
     touch([1886, 967])
@@ -72,13 +72,13 @@ def run_away():
     touch([1328, 751])
     sleep(1.2)
 
-def inte_fail():
+def inte_fail():    #网络异常
     if exists(Template(r"../tpl1589532705892.png", record_pos=(-0.001, 0.0), resolution=(2244, 1080))):
         touch([1323, 750])
         return True
     return False
 
-def item_detail():
+def item_detail():  #点到物品详细
     if exists(Template(r"../tpl1589627182286.png", record_pos=(-0.001, -0.111), resolution=(2244, 1080))):
         touch([1122, 762])
         return True
